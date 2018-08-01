4306aa1e7b68fc9ee67c7bbe81941e82202080e7
Yaqueline <yaqueline.hoyos@udea.edu.co>
2018-08-01 17:14:00 -0500

    Se ajusta ubicación del archivo index.html para evitar problemas con el express

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var io = require('socket.io')(http);
 
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+    console.log("nodo");
   });
 
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/frontend/index.html');;
+    res.sendFile(__dirname + '/index.html');;
 });
 
 http.listen(3000, function(){
