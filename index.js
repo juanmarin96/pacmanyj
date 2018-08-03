7986564f51bd113773f65d6a55caca1c57315c8a
juan_emg <esteban.marin@xpertgroup.co>
2018-08-02 19:30:38 -0500

    Se valida tamaño maximo de nodos por sala

--- index.js
+++ index.js
@@ -3,18 +3,31 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
-io.on('connection', function(socket){
-    socket.on('room', function(room) {
+io.on('connection', function (socket) {
+  socket.on('room', function (room) {
+
+    var rooms = io.sockets.adapter.rooms[room];
+    var numeroNodos = (rooms === undefined) ? 0 : rooms.length;
+    if (numeroNodos === 0) {
       socket.join(room);
-      io.sockets.in(room).emit('joined', room);
-    });
+      io.sockets.in(rooms).emit('joined', room);
+      console.log(numeroNodos)
+    }else if(numeroNodos < 4){
+      socket.join(room);
+      io.sockets.in(rooms).emit('joined', room);
+      console.log(numeroNodos)
+
+    }else{
+      socket.emit('full','Sala llena');
+    }
   });
-
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');;
 });
 
-http.listen(3000, function(){
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');;
+});
+
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
