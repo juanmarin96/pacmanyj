6b446951a56c27d05ee856a424819cd40ba4ad50
Yaqueline <yaqueline.hoyos@udea.edu.co>
2018-08-08 16:35:50 -0500

    Se estabiliza la creación de rooms para jugar sesiones en simultaneo

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var pacmanyj = require('./pacmanyj');
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');;
-});
+app.use(express.static('./'));
 
 http.listen(3000, function () {
   console.log('listening on *:3000');
 });
 
 io.on('connection', function (socket) {
-  console.log("Nodo conectado")
   pacmanyj.iniciarJuego(io, socket);
 });
--- pacmanyj.js
+++ pacmanyj.js
@@ -6,23 +6,66 @@ exports.iniciarJuego = function(sio, socket){
     io = sio;
     gameSocket = socket;
 
-    gameSocket.on('room', aniadirNodoALaSala)
+    gameSocket.on('hostCreateNewGame', hostCreateNewGame);
+    gameSocket.on('hostRoomFull', hostPrepareGame);
+    gameSocket.on('hostCountdownFinished', hostStartGame);
+    gameSocket.on('hostNextRound', hostNextRound);
+    gameSocket.on('playerJoinGame', playerJoinGame);
 };
 
-function aniadirNodoALaSala(data){
-    console.log(data);
-    var rooms = io.sockets.adapter.rooms[data];
-        var numeroNodos = (rooms === undefined) ? 0 : rooms.length;
-        if (numeroNodos === 0) {
-          this.join(data);
-          io.sockets.in(rooms).emit('joined', data);
-        }else if(numeroNodos < 4){
-          this.join(data);
-          io.sockets.in(rooms).emit('joined', data);
-        }else if(numeroNodos === 3){
-          this.join(data);
-          io.sockets.in(rooms).emit('start', data);
-        }else{
-          this.emit('full','Sala llena');
-        }
+function hostCreateNewGame(data){
+  var thisGameId = ( Math.random() * 100000 ) | 0;
+
+  // Return the Room ID (gameId) and the socket ID (mySocketId) to the browser client
+  this.emit('newGameCreated', {gameId: thisGameId, mySocketId: this.id});
+
+  // Join the Room and wait for the players
+  this.join(thisGameId.toString());
 }
+
+function hostPrepareGame(gameId) {
+  var sock = this;
+  var data = {
+      mySocketId : sock.id,
+      gameId : gameId
+  };
+  //console.log("All Players Present. Preparing game...");
+  io.sockets.in(data.gameId).emit('beginNewGame', data);
+}
+
+function hostStartGame(gameId){
+  console.log(gameId);
+}
+
+function hostNextRound(data){
+  console.log(data);
+}
+
+function playerJoinGame(data) {
+  // A reference to the player's Socket.IO socket object
+  var sock = this;
+  console.log("scok " + sock)
+  console.log("scok " + io)
+
+  // Look up the room ID in the Socket.IO manager object.
+  var room = io.sockets.adapter.rooms[data.gameId];
+
+  // If the room exists...
+  if( room != undefined ){
+      // attach the socket id to the data object.
+      data.mySocketId = sock.id;
+
+      // Join the room
+      sock.join(data.gameId);
+
+      console.log('Player ' + data.playerName + ' joining game: ' + data.gameId );
+
+      // Emit an event notifying the clients that the player has joined the room.
+      io.sockets.in(data.gameId).emit('playerJoinedRoom', data);
+
+  } else {
+      // Otherwise, send an error message back to the player.
+      this.emit('error',{message: "This room does not exist."} );
+  }
+}
+
